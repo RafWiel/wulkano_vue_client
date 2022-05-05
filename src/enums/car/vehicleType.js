11794ad64045bef84e81c19108670c9d20812ca5
RafalW <rafal.wielicki@gmail.com>
2022-05-05 09:04:40 +0200

    Update vehicleType.js

--- src/enums/car/vehicleType.js
+++ src/enums/car/vehicleType.js
@@ -2,19 +2,16 @@ const none = 0;
 const car = 1;
 const truck = 2;
 const offroad = 3;
-const cale = 4;
 
 module.exports = {
   none,
   car,
   truck,
   offroad,
-  cale,
   items: [
     { id: car, text: 'Osobowy' },
     { id: truck, text: 'Dostawczy' },
     { id: offroad, text: 'Terenowy' },
-    { id: cale, text: 'CALE' },
   ],
   getText(id) {
     const item = this.items.find((u) => u.id === id);
