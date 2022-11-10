a9c9827b572f731b07dbf0a9153c500307805578
RafalW <rafal.wielicki@gmail.com>
2022-11-10 08:04:35 +0100

    Update rules.js

--- src/misc/rules.js
+++ src/misc/rules.js
@@ -1,7 +1,7 @@
 module.exports = {
   required: (v) => !!v || 'Pole wymagane',
-  integer: (v) => !Number.isNaN(parseInt(v, 10)) || 'Wymagana wartość liczbowa',
-  float: (v) => !Number.isNaN(parseFloat(v, 10)) || 'Wymagana wartość liczbowa',
+  integer: (v) => Number.isInteger(v) || 'Wymagana wartość liczbowa',
+  float: (v) => Number(v) || 'Wymagana wartość liczbowa',
   email: (v) => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Nieprawidłowy adres e-mail',
   // eslint-disable-next-line
   phoneNumber: (v) => !v || /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/.test(v) || 'Nieprawidłowy numer telefonu',
